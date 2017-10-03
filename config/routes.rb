8304966e4c3a5f63b539a614aa3ef2063e581906
Matt <magius@uncc.edu>
2017-10-03 19:29:23 +0000

    Done with section 4.1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+Myrottenpotatoes::Application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
+end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end

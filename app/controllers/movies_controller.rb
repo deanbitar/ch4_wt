9fdfd0ff273537c851e4b9953142694d7b5c4472
Matt <magius@uncc.edu>
2017-10-03 20:39:57 +0000

    Done with section 4.5.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,10 +5,18 @@ class MoviesController < ApplicationController
   end
   # in app/controllers/movies_controller.rb
 
+=begin
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.html.haml by default
+  end
+=end
+  
+  # in controller action:
+  def show
+    @movie = Movie.where(:id => params[:id]) # what if this movie not in DB?
+    # BUG: we should check @movie for validity here!
   end
   
   # add below all other methods
